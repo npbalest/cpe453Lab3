a9d7cb57560b88d9f833c06a8cd25038b2ebd143
Acaste10 <acaste10@calpoly.edu>
2020-02-01 14:54:55 -0800

    done

--- multithread.c
+++ multithread.c
@@ -4,9 +4,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <pthread.h>
+#include <string.h>
 
 #define NUM_THREADS 2
-#define ARR_SIZE 20
+#define ARR_SIZE 3000000
 
 void merge(int array[], int left, int right, int middle);
 void *sorting(void *param);
@@ -14,12 +15,8 @@ int cmpfunc (const void * a, const void * b);
 void print_list(int array[], int size);
 
 int arr[ARR_SIZE] = {};
+int arr_copy[ARR_SIZE] = {};
 int threadpart = 0;
-
-//Need to do
-//dup so you can compare both runtimes
-//find arr to put in with fgets
-
 
 //Driver program
 int main(int argc, char* argv[])
@@ -30,7 +27,10 @@ int main(int argc, char* argv[])
 	in = fopen(argv[1], "r");
 	int i = 0;
 	int num;
-	int b = 0, c = 0; //for threads for loops
+	int b = 0, c = 0, n = 0; //for threads for loops
+
+	struct timespec start, start_1, finish, finish_1;
+	double elapsed, elapsed_1;
 
 	if (in == NULL) {
 		printf("No file to read in.");
@@ -43,11 +43,24 @@ int main(int argc, char* argv[])
 		fclose(in);
 	}
 
-	printf("Given array is \n"); 
-    print_list(arr, i); 
+ //    printf("Number of ints in file (i): %d\n", i); 
+
+	// printf("Given array is \n"); 
+ //    print_list(arr, i); 
 
 	pthread_t tid[NUM_THREADS];
 
+    memcpy(arr_copy, arr, i*sizeof(int));
+    clock_gettime(CLOCK_MONOTONIC, &start_1); 
+	qsort(arr_copy, i, sizeof(int), cmpfunc);
+	clock_gettime(CLOCK_MONOTONIC, &finish_1);
+	elapsed_1 = (finish_1.tv_sec - start_1.tv_sec);
+	elapsed_1 += (finish_1.tv_nsec - start_1.tv_nsec) / 1000000000.0;
+	printf("Time for sequential \n");
+	printf("%f\n", elapsed_1);
+
+
+	clock_gettime(CLOCK_MONOTONIC, &start); 
 
 	for (b = 0; b < NUM_THREADS; b++) {
 		pthread_create(&tid[b], NULL, sorting, (void*)(intptr_t)i);
@@ -57,13 +70,25 @@ int main(int argc, char* argv[])
 		pthread_join(tid[c], NULL);
 	}
 
-    printf("After threads array is \n"); 
-    print_list(arr, i);
+    // printf("After threads array is \n"); 
+    // print_list(arr, i);
 
-    merge(arr, 0, i, i/2);
+    merge(arr, 0, i-1, (i-1)/2);
 
-    printf("After merge \n"); 
-    print_list(arr, i);
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+	elapsed = (finish.tv_sec - start.tv_sec);
+	elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+	printf("Time for multithreading \n");
+	printf("%f\n", elapsed);
+
+	out = fopen("answer.txt", "w");
+	for(n = 0; n < i; n++) {
+		fprintf(out,"%d ",arr[n]);
+	}
+	fclose(out);
+
+    // printf("After merge \n"); 
+    //print_list(arr, i);
 
     return 0;
 } 
@@ -73,10 +98,16 @@ int cmpfunc (const void * a, const void * b) {
    return ( *(int*)a - *(int*)b );
 }
 
+//merge function to merge subarrays
 void merge(int array[], int left, int right, int middle) {
 	int a = 0, b = 0;
 	int i = middle - left + 1;
 	int j = right - middle;
+
+	// printf("right: %d\n", right);
+	// printf("mid: %d\n", middle);
+	// printf("i: %d\n", i); 
+	// printf("j: %d\n", j); 
 
 	int temp1[i];
 	int temp2[j];
@@ -87,6 +118,10 @@ void merge(int array[], int left, int right, int middle) {
 	for (b = 0; b < j; b++) {
 		temp2[b] = array[b + middle + 1];
 	}
+
+    // printf("Temp arrays are: \n"); 
+    // print_list(temp1, i);	
+    // print_list(temp2, j);
 
 	a = 0;
 	b = 0;
@@ -117,21 +152,40 @@ void merge(int array[], int left, int right, int middle) {
 
 
 void *sorting(void *param) {
-	int size = (intptr_t)param;
 	threadpart++;
 	if (threadpart == 1) {
-		printf("1st half\n");
-		print_list(arr, size/2);
-		qsort(arr, size/2, sizeof(int), cmpfunc);
-		printf("1st half sorted\n");
-		print_list(arr, size/2);
+		int size = (intptr_t)param;
+		// printf("1st half\n");
+		if(size % 2 != 0){
+			// print_list(arr, size/2+1);
+			qsort(arr, size/2+1, sizeof(int), cmpfunc);
+		} else {
+			// print_list(arr, size/2);
+			qsort(arr, size/2, sizeof(int), cmpfunc);
+		}
+		// printf("1st half sorted\n");
+		// if(size % 2 != 0){
+		// 	print_list(arr, size/2+1);
+		// } else {
+		// 	print_list(arr, size/2);
+		// }
 	}
 	else {
-		printf("2nd half\n");
-		print_list(&arr[size/2], size/2);
-		qsort(&arr[size/2], size/2, sizeof(int), cmpfunc);
-		printf("2nd half sorted\n");
-		print_list(&arr[size/2], size/2);
+		int size = (intptr_t)param;
+		// printf("2nd half\n");
+		if(size % 2 != 0){
+			// print_list(&arr[(size/2)+1], size/2);
+			qsort(&arr[(size/2)+1], size/2, sizeof(int), cmpfunc);
+		} else {
+			// print_list(&arr[size/2], size/2);
+			qsort(&arr[size/2], size/2, sizeof(int), cmpfunc);
+		}
+		// printf("2nd half sorted\n");
+		// if(size % 2 != 0){
+		// 	print_list(&arr[(size/2)+1], size/2);
+		// } else {
+		// 	print_list(&arr[size/2], size/2);
+		// }
 	}
 	pthread_exit(0);
 }
