b6cedc169166aa0eb9f9ef5615f97f537581ee74
Adrian <accpichardo@gmail.com>
2020-02-01 12:08:28 -0800

    dynamically allocate arrays

--- multithread.c
+++ multithread.c
@@ -1,75 +1,124 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <pthread.h>
-#include <time.h> 
 
-#define NUM_THREADS 2 
-#define ARR_SIZE 3000000
+#define NUM_THREADS 2
+#define ARR_SIZE 20
 
+void merge(int array[], int left, int right, int middle);
+void *sorting(void *param);
+int cmpfunc (const void * a, const void * b);
 void print_list(int array[], int size);
-void *sorting(void *param); //Threads call this function
 
-int arr[ARR_SIZE] = {}; //array shared by all threads
+int arr[ARR_SIZE] = {};
+int first_half[] = {};
+int second_half[] = {};
+int threadpart = 1;
 
-void print_list(int array[], int size) 
-{ 
+
+//Need to do
+//dup so you can compare both runtimes
+//find arr to put in with fgets
+
+
+//Driver program
+int main(int argc, char* argv[])
+{
+    FILE *in;
+    FILE *out;
+
+    in = fopen(argv[1], "r");
+    int i = 0;
+    int num;
+    int g, h, f; //for splitting arrays
+
+    if (in == NULL) {
+        printf("No file to read in.");
+    }
+    else {
+        while ((fscanf(in, "%d", &num) != EOF) && (i < ARR_SIZE)) {
+            arr[i] = num;
+            i++;
+        }
+        fclose(in);
+    }
+    //printf("%d\n", i);
+
+    printf("Given array is \n"); 
+    print_list(arr, i); 
+
+    int first_half[] = (int *)malloc(sizeof(int)*(i/2));
+    int second_half[] = (int *)malloc(sizeof(int)*(i/2));
+
+    if(first_array == NULL) {
+      printf("first malloc of size %d failed!\n", i);   // could also call perror here
+      exit(1);   // or return an error to caller
+    }
+
+    if(second_array == NULL) {
+      printf("second malloc of size %d failed!\n", i);   // could also call perror here
+      exit(1);   // or return an error to caller
+    }
+
+    while (g != i) {
+        //get first half of array
+        if (f < (i / 2)) {
+            first_half[f] = arr[f];
+            f++;
+        } //second half
+        else {
+            second_half[h] = arr[f];
+            h++;
+            f++;
+        }
+        g++;
+    }
+    print_list(first_half, f);
+    print_list(second_half, h);
+
+    pthread_t tid[NUM_THREADS];
+
+
+    for (int b = 0; b < NUM_THREADS; b++) {
+        pthread_create(&tid[b], NULL, sorting, NULL);
+    }
+
+    for (int c = 0; c < NUM_THREADS; c++) {
+        pthread_join(tid[i], NULL);
+    }
+
+    printf("\nSorted array is \n"); 
+    print_list(arr, i);
+  
+/*
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+    qsort(arr, 6, sizeof(int), cmpfunc);*/
+    return 0;
+} 
+
+
+void *sorting(void *param) {
+    if (threadpart == 1) {
+        threadpart++;
+        qsort(first_half, ( sizeof(first_half)/sizeof(first_half[0]) ), sizeof(int), cmpfunc);
+    }
+    else {
+        qsort(second_half, ( sizeof(second_half)/sizeof(second_half[0])), sizeof(int), cmpfunc);
+    }
+    pthread_exit(0);
+}
+
+
+//for builtin qsorting 
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+//want to print list to view
+void print_list(int array[], int size) { 
     int i; 
     for (i = 0; i < size; i++) 
         printf("%d ", array[i]); 
     printf("\n"); 
 } 
-  
-/* Driver program to test above functions */
-int main(int argc, char *argv[]) 
-{ 
-    int num, i;
-
-    FILE *in;
-
-    in = fopen(argv[1],"r");
-
-    //input numbers from file into array
-    while (!feof (in) && fscanf (in, "%d", &num) && i++ < NUM_THREADS){
-        arr[i] = num;
-    }
-
-    int arr_size = sizeof(arr)/sizeof(arr[0]);
-
-    pthread_t tid[NUM_THREADS]; //thread identifier
-    pthread_attr_t attr[NUM_THREADS]; //set of thread attributes
-
-    //initialize all thread attributes
-    for (i = 0; i < NUM_THREADS; i++){
-        pthread_attr_init(&attr[i]);
-    }
-
-    //creates all threads with appropiate arguments
-    for (i = 0; i < NUM_THREADS; i++){
-        pthread_create(&tid[i], &attr[i], sorting, tid[0]);
-    }
-
-    //joins all threads
-    for (i = 0; i < NUM_THREADS; i++){
-        pthread_join(tid[i], NULL);
-    }
-
-    print_list(arr, arr_size); 
-
-    fclose(in);
-
-    return 0; 
-}
-
-//Threads will begin control in this function
-void *sorting(void *param)
-{
-    if (pthread_self() == (int)param) {
-        //lower bound
-    } else {
-        //upper bound
-    }
-
-    pthread_exit(0);
-}
